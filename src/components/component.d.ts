eb4c62cf23b1cb07bc638af022d1e16094210d5d
zy <1824762582@qq.com>
2024-05-01 23:33:50 +0800

    猜你喜欢组件的封装

--- src/components/component.d.ts
+++ src/components/component.d.ts
@@ -1,7 +1,10 @@
 import XtxSwiper from '@/components/XtxSwiper.vue'
+import XtxGuess from '@/components/XtxGuess.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
     // 前面是名称后面是类型
     XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -38,16 +38,31 @@ onLoad(() => {
 
 <template>
   <CustomNavbar />
-  <!--  传送数组-->
-  <XtxSwiper :list="bannerList" />
-  <!--  分类面板-->
-  <CategoryPanel :list="categoryList" />
-  <!--  热门推荐-->
-  <HotPanel :list="hotList" />
+  <!--  滚动容器-->
+  <scroll-view class="scroll-view" scroll-y>
+    <!--  传送数组，轮播图-->
+    <XtxSwiper :list="bannerList" />
+    <!--  分类面板-->
+    <CategoryPanel :list="categoryList" />
+    <!--  热门推荐-->
+    <HotPanel :list="hotList" />
+    <!--  猜你喜欢-->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
-.page {
+// 不能加.,在page
+page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  // 定义垂直方向
+  flex-direction: column;
+}
+
+.scroll-view {
+  // 分配了伸缩因子为1，它会在父容器中占据剩余的可用空间，使得它在垂直方向上填充整个可用空间
+  flex: 1;
 }
 </style>
