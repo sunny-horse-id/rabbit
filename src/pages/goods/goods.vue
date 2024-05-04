3beba8618b4e7e35844ece0df1dcc56a17c26d77
zy <1824762582@qq.com>
2024-05-04 22:18:54 +0800

    购物车模块-两个购物车页面

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -235,7 +235,7 @@ const onAddCart = async (ev: SkuPopupEvent) => {
         <text class="icon-handset"></text>
         客服
       </button>
-      <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
+      <navigator class="icons-button" url="/pages/cart/cart2" open-type="navigate">
         <text class="icon-cart"></text>
         购物车
       </navigator>
